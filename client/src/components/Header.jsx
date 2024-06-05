a865fc878b5fcee2e602e1a2711adc982a1201b7
Mdkaif001 <mkkaif002@gmail.com>
2024-06-05 17:04:37 +0530

    Kaif | Add : Header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Logo from './Logo'
+import { CiSearch } from "react-icons/ci";
+import { FaShoppingCart, FaUserCircle } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className='h-16 shadow-md'>
+      <div className='container mx-auto h-full flex items-center px-4 justify-between'>
+            <div>
+                <Link to={"/"}>
+                <Logo h={60} w={50}/>
+                </Link>
+            </div>
+            <div className='flex items-center'>
+                <input type='text' placeholder='Search product here...'/>
+                <div>
+                    <CiSearch/>
+                </div>
+            </div>
+            <div className='flex items-center'>
+               <div>
+               <FaUserCircle/>
+               </div>
+               <div>
+                <span><FaShoppingCart/></span>
+                <div>
+                    <p className='text-xs'>
+                        0
+                    </p>
+                </div>
+               </div>
+               <div>
+                <button>
+                    <Link to={'/login'}>
+                    Login
+                    </Link>
+                </button>
+               </div>
+            </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
